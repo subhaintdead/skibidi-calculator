e451b5ffc006030448ccf46d7a447600ede03b26
subhaintdead <subhgotbeans@gmail.com>
2026-09-17 19:59:45 +0530

    v2 roughly done

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# this is gonna be some calculator that gives a different answer than it should using some gibberish formula
+import math
 
 
 def legit(a, b, operator):
@@ -14,51 +14,78 @@ def legit(a, b, operator):
         else:
             return a / b
     else:
-        return "gng that aint a operator"
+        return None
 
 
 # now the skibidi calculation
-
-
-def dsum(n):  # digits sum, in case i forget it later
-    total = 0
-    for banana in str(int(n)):
-        total += int(banana)
-    return total
+def skibidi(a, b):
+    banana = a * math.pi + math.sqrt(2)
+    wave = math.sin(banana) * math.cos(banana * math.sqrt(3))  # i hate trigonometry
+    return wave
 
 
 def skibidimath(a, b, operator, legitnum):
 
     # rizz time
-
     rizz = (
-        dsum(a) * 6.8 + dsum(b) * 3 - 6.7
-    ) % 13  # 6.8 specifically cuz 68 is the number between six-seven and 69 divided by 10
+        skibidi(a, b) * 6.8
+    )  # ig the number might be too small otherwise, or even if it is a lil too big, doesnt matter
 
     if operator == "+":
-        skibidinum = legitnum + rizz - 5
+        skibidinum = legitnum + rizz
     elif operator == "-":
-        skibidinum = legitnum - rizz + 3
+        skibidinum = legitnum - rizz
     elif operator == "/":
-        skibidinum = legitnum + (rizz / 4) + 1.5
+        skibidinum = legitnum + (rizz / 4)
     elif operator == "*":
-        skibidinum = legitnum + (rizz * 4) - 2
+        skibidinum = legitnum + (rizz * 2)
     else:
         skibidinum = legitnum
     return skibidinum
+
+
+banner = r""""
+
+ ____   _     _  _      _      _  _                     
+/ ___| | | __(_)| |__  (_)  __| |(_)                    
+\___ \ | |/ /| || '_ \ | | / _` || |                    
+ ___) ||   < | || |_) || || (_| || |                    
+|____/ |_|\_\|_||_.__/ |_| \__,_||_|                    
+  ____        _               _         _               
+ / ___| __ _ | |  ___  _   _ | |  __ _ | |_  ___   _ __ 
+| |    / _` || | / __|| | | || | / _` || __|/ _ \ | '__|
+| |___| (_| || || (__ | |_| || || (_| || |_| (_) || |   
+ \____|\__,_||_| \___| \__,_||_| \__,_| \__|\___/ |_|   
+
+"""
+
+version = "v2.0.0"
+
+
+print(banner)
+print(f"skibidi calculator {version}, the least accurate arithmetic calculator")
+print("=" * 60)
 
 
 a = int(input("enter the first number: "))
 operator = input("enter the operator: ")
 b = int(input("enter the second number: "))
 
+print("-" * 60)
+
 if operator not in ["+", "-", "*", "/"]:
     print("invalid operator")
+else:
+    legitnum = legit(a, b, operator)
 
-legitnum = legit(a, b, operator)
+    if operator == "/" and b == 0:
+        print("bro you cant divide by 0(even the fakest clculator has limits gng)")
+    else:
+        skibidinum = skibidimath(a, b, operator, legitnum)
+        skibidinum = round(skibidinum, 2)
+        if skibidinum == legitnum:
+            skibidinum += 1  # edge case
+        print(f"the answer is: {skibidinum}")
+        print("run successfully. run again for some more brain-smoothening math")
 
-skibidinum = skibidimath(a, b, operator, legitnum)
-skibidinum = round(skibidinum, 2)
-if skibidinum == legitnum:
-    skibidinum += 1
-print(skibidinum)
+print("=" * 60)
